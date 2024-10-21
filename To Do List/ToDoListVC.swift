393f6498a1d82f7b7cbd75c155df09b64a5a33c8
Aytac Bulanik <aytacbulanik@hotmail.com>
2024-10-22 00:21:29 +0300

    tableview accessory type added

--- To Do List/ToDoListVC.swift
+++ To Do List/ToDoListVC.swift
@@ -21,9 +21,20 @@ class ToDoListVC: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "ToDoItemCell")
         cell.textLabel?.text = shoppingList[indexPath.row]
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
 
